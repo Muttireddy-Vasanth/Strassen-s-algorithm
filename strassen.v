633775c991d7cade74b879b5b3bd9c197f935c84
Muttireddy-Vasanth <vasanth.muttireddy@gmail.com>
2025-10-02 12:27:48 +0530

    Update strassen.v

--- strassen.v
+++ strassen.v
@@ -51,3 +51,16 @@ module strassen #(
     assign C22 = M1 - M2 + M3 + M6;
 
 endmodule
+
+
+module multiplier #(
+    parameter WIDTH = 16
+)(
+    input  wire [WIDTH-1:0] a,
+    input  wire [WIDTH-1:0] b,
+    output wire [2*WIDTH-1:0] product
+);
+
+    assign product = a * b;
+
+endmodule
